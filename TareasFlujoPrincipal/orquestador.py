bb0b35d28a5db60bb36f4f3ca3aadf6f387c451a
Roberto <robertocarlosjimenezrodriguez1@gmail.com>
2025-09-15 12:32:40 -0600

    Version 1 orquestador

--- TareasFlujoPrincipal/orquestador.py
+++ TareasFlujoPrincipal/orquestador.py
@@ -3,7 +3,12 @@ import time
 import ModulosGenerales.modulo_logging as modulo_logging 
 from ultralytics import YOLO
 from cameras_module import esta_trabajando,toma_frame
+from TareasFlujoPrincipal.audio_module import AudioSystem
 import cv2
+
+model = YOLO("best.pt")  # modelo YOLO
+roi_x1, roi_y1, roi_x2, roi_y2 = 320, 0, 640, 480   # Definir la zona de interés (x1, y1, x2, y2)
+
 
 #Declaracion de variables
 camara1 = cv2.VideoCapture(0)
@@ -16,7 +21,7 @@ from TareasSegundoPlano import oled_module
 modulo_logging.setup_logging()
 logger = logging.getLogger("snow").getChild("orchestrator")
 
-def run(stop_event,cola_frames):
+def run(stop_event,cola_frames,cola_audio):
 
     
     """
@@ -29,11 +34,34 @@ def run(stop_event,cola_frames):
     while not stop_event.is_set():
         try:
             if esta_trabajando:
-                logger.info("Module 'cameras_module' started")
                 toma_frame(camara1, camara2, cola_frames)
+
+
+                frame = cola_frames.get()  # Espera hasta que haya un frame disponible en la cola
+                
+                frame_roi = frame[roi_y1:roi_y2, roi_x1:roi_x2] # Extraer el ROI (recorte del frame)
+
+                frame_with_roi = frame.copy()   # La funcion copy realiza una copia de frame para evitar q al modificar alguno el otro se modifique
+                cv2.rectangle(frame_with_roi, (roi_x1, roi_y1), (roi_x2, roi_y2), (0, 255, 0), 2)
+
+
+                results = model(frame_roi)                          # YOLO espera un array tipo imagen
+
+                for box in results[0].boxes:    # Bucle de detecciones      results[0] indica la imagen actual      .boxes indica las bounding box
+                    conf = float(box.conf[0])           # Guarda la confianza de las bounding box
+                    if conf > 0.85:                     # si la conf es mayor a 84% reproduce un sonido y detiene el sistema hasta que termine este
+                        logging.info(f"Clase detectada con {conf*100:.2f}% de confianza")
+                        audio = AudioSystem(audio_file="audio.mp3")
+                        if not cola_audio.empty():
+                            señal = cola_audio.get()
+                            if señal == "play_audio":
+                                audio.activar_reproduccion
+                        audio.gestionar_cola()
+                        time.sleep(0.1)
+
             else:
                 time.sleep(2)
-
+            
         except Exception as e:
             logger.error(f"Error en el orquestador: {e}")
             
