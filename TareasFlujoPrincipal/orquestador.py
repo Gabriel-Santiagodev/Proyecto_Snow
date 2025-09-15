f72da7af7e7194975bbeff00c01b8920cee0cedd
Roberto <robertocarlosjimenezrodriguez1@gmail.com>
2025-09-15 11:45:28 -0600

    Merge2

--- TareasFlujoPrincipal/orquestador.py
+++ TareasFlujoPrincipal/orquestador.py
@@ -1,6 +1,14 @@
 import logging
 import time
 import ModulosGenerales.modulo_logging as modulo_logging 
+from ultralytics import YOLO
+from cameras_module import esta_trabajando,toma_frame
+import cv2
+
+#Declaracion de variables
+camara1 = cv2.VideoCapture(0)
+camara2 = cv2.VideoCapture(1)
+
 """
 from TareasFlujoPrincipal import cameras_module, yolo_module, audio_module, 
 from TareasSegundoPlano import oled_module
@@ -8,7 +16,9 @@ from TareasSegundoPlano import oled_module
 modulo_logging.setup_logging()
 logger = logging.getLogger("snow").getChild("orchestrator")
 
-def run(stop_event):
+def run(stop_event,cola_frames):
+
+    
     """
     Orchestrator: coordina el flujo entre cámaras, YOLO
     y audio, con o sin tracking.
@@ -18,50 +28,12 @@ def run(stop_event):
 
     while not stop_event.is_set():
         try:
-            """
-            CAMARAS -> YOLO -> AUDIO
-
-            toma_tu_frame = cameras_module.get_Frame()
-            if toma_tu_frame is None:
-                logger.info("No se pudo obtener el frame de la cámara")
-                continue
-            
-            frames_analizados = yolo_module.analyze_frame(toma_tu_frame)
-
-            if not frames_analizados:
-                logger.info("No se detectaron objetos en el frame")
-                continue
-            
-            if frames_analizados.get("confianza", 0) > 0.85:
-                audio_module.play_audio()
-                logger.info("Audio reproduciendose")
+            if esta_trabajando:
+                logger.info("Module 'cameras_module' started")
+                toma_frame(camara1, camara2, cola_frames)
             else:
-                logger.info("Confianza insuficiente, no se reproduce audio")
-            
-            time.sleep(0.1)  
-            """
-            # Con tracking--------------------------------------------------------------------
-            """
-            CAMARAS -> YOLO -> TRACKING -> AUDIO
+                time.sleep(2)
 
-            toma_tu_frame = cameras_module.get_Frame()
-            if toma_tu_frame is None:
-                logger.info("No se pudo obtener el frame de la cámara")
-                continue
-            
-            frames_analizados = yolo_module.analyze_frame(toma_tu_frame)
-
-            if not frames_analizados:
-                logger.info("No se detectaron objetos en el frame")
-                continue
-                
-            decision_tracking = tracking_module.apply_tracking(frames_analizados)
-            if decision_tracking:
-                audio_module.play_audio()
-                logger.info("Audio reproduciendose")
-            else:
-                logger.info("Tracking no aceptado, no se reproduce audio")
-            """
         except Exception as e:
             logger.error(f"Error en el orquestador: {e}")
             
